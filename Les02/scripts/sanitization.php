804642eb787487862c94e6f8c6d3188336a75264
ContardiC <c.contardi@icloud.com>
2022-10-25 17:39:31 +0200

    sanitizeString function added

--- Les02/scripts/sanitization.php
+++ Les02/scripts/sanitization.php
@@ -1,7 +1,16 @@
 <?php
+
+    function sanitizeString($data)
+    {
+        $data = trim($data);            //rimuove gli spazi prima e dopo la stringa 
+        $data = stripslashes($data);    // rimuove gli slashes //
+        $data=(filter_var($data, FILTER_SANITIZE_STRING));
+        return $data;
+    }
+ 
     $email=$_POST['email'];
     $password=$_POST['password'];
-    $emailSanitizzata = filter_var($email, FILTER_SANITIZE_EMAIL);
-    echo $emailSanitizzata;
-
+    $password_filtered=sanitizeString($password);
+    $email_filterd = filter_var($email, FILTER_SANITIZE_EMAIL);
+    echo $email_filtered;
 ?>
