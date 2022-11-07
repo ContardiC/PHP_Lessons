70142ff718e517f803b91bfe450bc39370f635b5
ContardiC <c.contardi@icloud.com>
2022-11-07 18:31:44 +0100

    db_connect added

---
+++ Less03/db_connect.php
@@ -0,0 +1,16 @@
+<?php
+$hostname="localhost";
+$username="root";
+$password="root";
+$database="news";
+$port="8889";
+// creo la connessione
+$conn=new mysqli($hostname,$username,$password,$database,$port);
+
+
+// controllo la connessione
+if ($conn->connect_error) {
+  die("Connessione fallita: " . $conn->connect_error);
+} 
+echo "Connessione ok";
+?>
