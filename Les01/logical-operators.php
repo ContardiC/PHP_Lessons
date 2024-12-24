9a28ec236382eef3570ce9b72aae036132834e6f
Carlo Contardi <c.contardi@spacecoding.it>
2024-12-24 11:44:36 +0100

    Add logical operators example

--- Les01/logical-operators.php
+++ Les01/logical-operators.php
@@ -1,6 +1,14 @@
 <?php
     $firstCondition=true;
     $secondCondition=false;
-    //TODO: to complete
+    //Logical operators
+    $result=$firstCondition && $secondCondition;
+    echo "AND: $result <br>";
+    $result=$firstCondition || $secondCondition;
+    echo "OR: $result <br>";
+    $result=!$firstCondition;
+    echo "NOT: $result <br>";
+    $result=$firstCondition xor $secondCondition;
+    echo "XOR: $result <br>";
     
 ?>
