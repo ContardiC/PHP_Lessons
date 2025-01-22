b0032d28200af2bb445adb0b1db96c46cc70a9b0
Carlo Contardi <c.contardi@spacecoding.it>
2025-01-22 17:10:48 +0100

    Add finding-characters functions

---
+++ Les01/finding-characters.php
@@ -0,0 +1,19 @@
+<?php
+$text = 'Home sweet home';
+?>
+<b>First match (case-sensitive):</b>
+<?= strpos($text,'ho') ?><br>
+<b>First match (not case-sensitive):</b>
+<?= strpos($text,'me', 5) ?><br>
+<b>Last match (case-sensitive):</b>
+<?= strrpos($text,'Ho') ?><br>
+<b>Last match (not case-sensitive):</b>
+<?= strripos($text,'Ho') ?><br>
+<b>Text after first match (case-sensitive)</b>
+<?= strstr($text,'ho') ?><br>
+<b>Text after first match (not case-sensitive)</b>
+<?= stristr($text,'ho') ?><br>
+<b>Text between two positions</b>
+<?= substr($text, 5, 5) ?><br>
+<?= 'hello'?><br>
+<?= $text ?>
